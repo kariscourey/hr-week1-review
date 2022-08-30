cc8e8c501854aed4eb693a3f21a863babcb5189d
karis courey <kariscourey@gmail.com>
2022-08-29 23:50:39 -0400

    add review

---
+++ part_1.py
@@ -0,0 +1,44 @@
+"""
+
+Data types
+
+Numeric data types:
+    Int - 1
+    Float - 1.0
+
+Dictionaries - {}
+
+Booleans - True/False
+
+Sequence Types
+    List - {}
+    Tuple - ()
+    Set - ()
+    Strings
+
+"""
+
+"""
+Operators
+=  --  assignment operator
++  --  1 + 1 or we can concetanate strings together
+-  --  subtraction
+*  --  multiplication
+/  --  division
+>  --  greater than
+<  --  less than
+>=  -  greater than or equal to
+<=  -  less than or equal to
+==  -  equal to
+/=  -  divide equals
++=  -  plus equals
+*=  -  multiply equals
+-=  -  subtract equals
+
+"""
+
+
+
+# Variables
+
+name = "jay"
